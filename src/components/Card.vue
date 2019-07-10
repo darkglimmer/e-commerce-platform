dc78f196c7af08cde25e92b86814d07a6b77408c
ShiinaOrez <shiina_orez@qq.com>
2019-07-10 15:05:12 +0800

    [fixed]

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="card-productNote">
       <div class="card-img">
-        <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3196541190,1207059702&fm=26&gp=0.jpg" style="height: 400px; width: 400px;">
+        <img src="this.imageURL" style="height: 400px; width: 400px;">
       </div>
       <div class="card-information">
         <div class="card-title">
-          <span style="font-size: 28px; font-weight: bold;">JOJO的奇妙冒险 阿帕茶意大利独家</span>
+          <span style="font-size: 28px; font-weight: bold;">{{this.name}}</span>
           <el-rate
             v-model="value5"
             disabled
@@ -18,12 +18,7 @@
         <hr width=100% size=2 color=#B0B0B0 style="filter:progid:DXImageTransform.Microsoft.Glow(color=#B0B0B0); margin-top: 10px;"> 
         <div class="card-description"> 
           <p style="font-size: 18px; line-height: 25px;">
-            <i>&nbsp; &nbsp;茸茸同款推荐！水母超爱喝！</i>
-            <br>
-            <i>味道浓郁，回味芳香！</i>
-            <br>
-            <i>本产品由 雷欧 阿帕奇 独家制造。警署专供。</i>
-            <br>
+            <i>{{this.desc}}</i>
           </p>
         </div>
         <div class="card-share">
@@ -32,7 +27,7 @@
           <img src="../assets/share.png" style="margin-left: 40px;">
         </div>
         <div class="card-statement">
-          <span>若引用后引起不是。概不负责～</span>
+          <span>请仔细阅读《免责声明》</span>
         </div>
       </div>
       <div class="card-shopping">
@@ -47,14 +42,14 @@
             </el-button>
         </div>
         <div class="card-price">
-          <span style="font-size: 30px; font-weight: bold; color: red;">$ {{this.num1*999.99}}</span>
+          <span style="font-size: 30px; font-weight: bold; color: red;">$ {{this.num1*(price-1)}}</span>
           <br>
-          <span style="text-decoration: line-through; font-size: 22px; line-height: 25px; color: #707070">$ {{this.num1*1000.00}}</span>
+          <span style="text-decoration: line-through; font-size: 22px; line-height: 25px; color: #707070">$ {{this.num1*this.price}}</span>
         </div>
         <div class="card-quantity">
           <span style="font-size: 15px; font-weight: bold;">数量:</span>
         </div>
-        <el-input-number v-model="num1" @change="handleChange" size="mini" :min="1" :max="99" label="描述文字" style="margin-left: 30px;"></el-input-number>
+        <el-input-number v-model="num1" @change="handleChange" size="mini" :min="1" :max="this.num" label="描述文字" style="margin-left: 30px;"></el-input-number>
         <div class="card-add">
           <el-button type="danger" size="small" class="card-info-button" round @click="login">添加到购物车</el-button>
         </div>
@@ -66,6 +61,13 @@
 export default {
   name: 'productNote',
   props: {
+    id: Number,
+    price: Number,
+    num: Number,
+    categoryid: Number,
+    name: String,
+    desc: String,
+    imageURL: String
   },
   data () {
     return {
@@ -78,12 +80,11 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .card-productNote{
   height: 400px;
   vertical-align: top;
   vertical-align: bottom;
-  margin: 300;
 }
 .card-img{
   display: inline-block;
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -207,6 +207,9 @@ export default {
         },
         deleteAll(){
             this.tableData=[];
+        },
+        checked() {
+            
         }
     }
 }
