775ff285267273fb74b7d4d779632f024247e701
darkglimmer <2392897209@qq.com>
2019-07-11 17:39:38 +0800

    feat: fix register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,15 +7,15 @@
         <div class="login-card">
             <div class="login-card-title">Welcome to register</div>
             <el-form>
-                <div class="login-message">用户名:<el-input v-model="username"></el-input></div>
-                <div class="login-message">密码:<el-input v-model="password"></el-input></div>
+                <div class="login-message">用户名:<el-input v-model="name"></el-input></div>
+                <div class="login-message">密码:<el-input v-model="passwordhash"></el-input></div>
                 <div class="login-message">设置密保问题:<el-input v-model="question"></el-input></div>
                 <div class="login-message">你的回答:<el-input v-model="answer"></el-input></div>
                 <div class="login-message">性别: 
                     <div class="login-message">
                         <el-select v-model="sex" placeholder="" style="width:350px">
-                            <el-option label="女" value="0"></el-option>
-                            <el-option label="男" value="1"></el-option>
+                            <el-option label="女" value="false"></el-option>
+                            <el-option label="男" value="true"></el-option>
                         </el-select>
                     </div>
                 </div>
@@ -31,7 +31,7 @@
                 </div>
                 <div class="login-message">手机:<el-input v-model="phone"></el-input></div>
                 <div class="login-message">邮箱:<el-input v-model="mail"></el-input></div>
-                <el-button type="danger" class="login-button" round>注册</el-button>
+                <el-button type="danger" class="login-button" round @click="register">注册</el-button>
             </el-form>
             <div class="login-tips">已有账号，立刻 <span style="cursor:pointer;" @click="toLogin" class="register">登录</span></div>
         </div>
@@ -45,6 +45,7 @@
 <script>
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
+import { constants } from 'crypto';
 
 export default {
   name: 'register',
@@ -54,8 +55,8 @@ export default {
   },
   data(){
     return {
-        username:'',
-        password:'',
+        name:'',
+        passwordhash:'',
         question:'',
         answer:'',
         sex: null,
@@ -67,6 +68,33 @@ export default {
   methods:{
       toLogin(){
           this.$router.push("/login")
+      },
+      register(){
+          const data = {
+            "id": 111,
+            "name": this.name,
+            "passwordhash": this.passwordhash,
+            "question": this.question,
+            "answer": this.answer,
+            "sex": this.sex,
+            "birth": this.birth,
+            "phone": this.phone,
+            "mail": this.mail
+          }
+           fetch(`/api/login?action=registed`, {
+                method: 'POST',
+                headers: {
+                "Content-Type":"appliation/json",
+                },
+                body: JSON.stringify(data)
+            }).then(res => {
+                if (res.ok){
+                    window.alert('注册成功！')
+                    this.$router.push("/login")
+                }else{
+                    window.alert('注册失败！')
+                }
+            })
       }
   }
 }
